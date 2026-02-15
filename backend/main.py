32d9443340bfef61a56e5100f833d664d459c763
Krish-Om <Krishombasukala@gmail.com>
2026-02-15 18:21:04 +0545

    chore: refactored code for swagger docs

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,19 @@
 from fastapi import FastAPI
-app = FastAPI()
+
+description = """
+Healthcare Self-Report vs Metrics API
+
+* **Patient-reported symptoms** (text input)
+
+* **Clinical features** (cholesterol, BP, age)
+
+Your AI detects mismatch risk.
+"""
+app = FastAPI(
+    title="Contradiction-Detection-System-API", description=description, version="0.0.1"
+)
+
 
 @app.get("/")
 def main():
-    return {"message": "Hello World"}
+    return {"message": "🚀🚀🚀API is running🚀🚀🚀"}
